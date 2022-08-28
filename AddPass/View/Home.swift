d865fe33940434e67c304afd1ba973328b696e9a
phib <phil@blais.dev>
2022-08-27 17:36:34 -0700

    Add View animations, design tweaks

--- AddPass/View/Home.swift
+++ AddPass/View/Home.swift
@@ -8,21 +8,48 @@
 import SwiftUI
 
 struct Home: View {
+    @State var expandCards: Bool = false
     var body: some View {
-        VStack {
-            Text("Wallet").font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity, alignment: .center)
+        VStack(spacing: 0) {
+            Text("Wallet")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: expandCards ? .leading : .center)
+                .overlay(alignment: .trailing) {
+                    Button {
+                        withAnimation(.interactiveSpring(response: 0.8, dampingFraction: 0.7,
+                                blendDuration: 0.7)) {
+                            expandCards = false
+                        }
+                    } label: {
+                        Image(systemName: "plus")
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .background(.blue, in: Circle())
 
+                    }
+                            .rotationEffect(.init(degrees: expandCards ? 45 : 0))
+                            .offset(x: expandCards ? 10 : 15)
+                            .opacity(expandCards ? 1 : 0)
+                }.padding(.horizontal, 15)
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 0) {
                     ForEach(cards) { card in
-
                         CardView(card: card)
                     }
                 }
+                .overlay {
+                    Rectangle().fill(.black.opacity(expandCards ? 0 : 0.01)).onTapGesture {
+                        withAnimation(.easeInOut(duration: 0.35)) {
+                            expandCards = true
+                        }
+                    }
+                }
+                .padding(.top, expandCards ? 30 : 0)
             }
+            
             .coordinateSpace(name: "SCROLL")
+            .offset(y: expandCards ? 30 : 0)
         }
         .padding([.horizontal, .top])
     }
@@ -31,39 +58,43 @@ struct Home: View {
     func CardView(card: Card) -> some View {
         GeometryReader { proxy in
             let rect = proxy.frame(in: .named("SCROLL"))
-            let offset = -rect.minY + CGFloat(getIndex(Card: card) * 70)
+            let offset = CGFloat(getIndex(Card: card) * (expandCards ? 10 : 70))
             ZStack(alignment: .bottomLeading) {
-                
+
                 Image(card.cardImage)
                         .resizable()
-                        .aspectRatio(contentMode: .fit)
-                
+                        .scaledToFill()
+                        .shadow(color: .gray, radius: 4, x: 1, y: 1)
+                        .padding(5)
+
                 VStack(alignment: .leading, spacing: 10) {
                     Text(card.name).fontWeight(.bold)
-                    
-                    Text(customizedCardNumber(number:card.cardNumber))
-                        .font(.callout)
-                        .fontWeight(.bold)
-                }.padding()
-                .padding(.bottom, 10)
-                .foregroundColor(.black)
-                
-            }.offset(y: offset)
+
+                    Text(customizedCardNumber(number: card.cardNumber))
+                            .font(.callout)
+                            .fontWeight(.bold)
+                }
+                        .padding()
+                        .padding(.bottom, 10)
+                        .foregroundColor(.black)
+
+            }
+                    .offset(y: expandCards ? offset : -rect.minY + offset)
         }
-        .frame(height: 200)
+        .frame(width: .infinity, height: 235)
     }
-    
-    func getIndex(Card: Card)->Int{
+
+    func getIndex(Card: Card) -> Int {
         return cards.firstIndex { currentCard in
             return currentCard.id == Card.id
         } ?? 0
     }
-    
-    func customizedCardNumber(number: String)-> String{
-        
+
+    func customizedCardNumber(number: String) -> String {
+
         var newValue: String = ""
         let maxCount = number.count - 4
-        
+
         number.enumerated().forEach { value in
             if value.offset >= maxCount {
                 let string = String(value.element)
@@ -74,10 +105,10 @@ struct Home: View {
                     newValue.append(contentsOf: " ")
                 } else {
                     newValue.append(contentsOf: "*")
-                    
+
                 }
             }
-            
+
         }
         return newValue
     }
